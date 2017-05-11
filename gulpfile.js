5859f0729064d0945b8406c3b4f6ada5f8fea34b
Ron Marx <ronmarx@Administrators-MacBook-Pro.local>
2017-05-11 17:24:16 -0600

    Finalize reactpractice. Toastr is not working, and toastr.css is missing from the toastr root

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,16 @@ var lint = require('gulp-eslint');
 
 
 var config = {
-    port: 9005,
+    port: 10303,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        images: './src/images/*',
         css:[
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
-            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+            'node_module/toastr/toastr.scss'
         ],
         dist: './dist',
         mainJs: './src/main.js'
@@ -62,6 +64,18 @@ gulp.task('css', function(){
         .pipe(gulp.dest(config.paths.dist + '/css'))
 });
 
+// Migrates images to dist folder
+// Note that you can optimize images here
+gulp.task('images', function(){
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/images'))
+        .pipe(connect.reload());
+
+    //publish favicon
+    gulp.src('./src/favicon.ico')
+        .pipe(gulp.dest(config.paths.dist));
+})
+
 gulp.task('lint', function(){
     return gulp.src(config.paths.js)
         .pipe(lint({config: 'eslint.config.json'}))
@@ -74,4 +88,4 @@ gulp.task('watch', function(){
 });
 
 
-gulp.task('default',['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task('default',['html', 'js', 'css', 'images', 'lint', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-$ = jQuery = require('jquery');
+"use strict";
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./routes');
+
+Router.run(routes, function(Handler){
+    React.render(<Handler/>, document.getElementById('app'));
+});
 
 
-var App = console.log('Hello world from Browserify');
-
-module.exports = App;
